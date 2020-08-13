b5696ce785fa44dd0cbe2fa18b3175493a61feca
rmatthew04 <47575136+rmatthew04@users.noreply.github.com>
2020-08-13 19:15:49 -0400

    Usage of Dot Notation as the best method to code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+let person = {
+  name: "Rach",
+  age: 37,
+};
+
+//Dot Notation (Best Method)
+person.name = "Dave";
+
+console.log(person.name);
